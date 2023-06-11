cb84577d23fb1762a5cb8e69b90c41fb56e3ea06
Ar7eniyan <ar7eniy.k@gmail.com>
2023-06-11 14:29:47 +0300

    Use criterion.rs to benchmark against built-in sin/cos

---
+++ benches/cordic_benchmark.rs
@@ -0,0 +1,39 @@
+use std::f64::consts::PI;
+
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId, BatchSize};
+use cordic::{precompute, sincos};
+use rand::prelude::*;
+
+fn get_random_angle90() -> f64 {
+    (random::<f64>() - 0.5f64) * PI
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    unsafe { precompute() };
+    let mut group = c.benchmark_group("f64 sin and cos");
+
+    for iterations in (5..=60).step_by(5) {
+        group.bench_function(
+            BenchmarkId::new("cordic", iterations.to_string() + " iterations"),
+            |b| b.iter_batched(
+                || get_random_angle90(),
+                |angle| sincos(angle, iterations),
+                BatchSize::SmallInput
+            )
+        );
+    }
+    
+    group.bench_function(
+        "std",
+        |b| b.iter_batched(
+            || get_random_angle90(),
+            |angle| (angle.sin(), angle.cos()),
+            BatchSize::SmallInput
+        )
+    );
+
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
